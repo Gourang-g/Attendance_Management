bde50bef1af9d95f009349b5f0400f6d4869c1df
Gourang-g <gouranggangrade382004@gmail.com>
2026-04-16 18:27:22 +0530

    attendance repository

--- src/main/java/Project/Attendance/Repository/AttendanceRepository.java
+++ src/main/java/Project/Attendance/Repository/AttendanceRepository.java
@@ -1,5 +1,20 @@
 package Project.Attendance.Repository;
 
+import Project.Attendance.Model.Attendance;
+import Project.Attendance.Model.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttendanceRepository {}
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findByStudent(Student student);
+    List<Attendance> findAttendanceByDate(LocalDate date);
+    List<Attendance> findByStudentAndDate(Student student, LocalDate date);
+    List<Attendance> findByStudent_ClassEntity_Id(Long classId);
+    Optional<Attendance> findByStudentAndDateAndSubject(
+            Student student, LocalDate date, String subject
+    );
+    boolean existsByStudentAndDate(Student student, LocalDate date);
+}
