d541b99e045635693a93c1de73a259956ccd3087
Gourang-g <gouranggangrade382004@gmail.com>
2026-05-06 18:19:39 +0530

    Controller student created

---
+++ src/main/java/Project/Attendance/Backend/Controller/StudentController.java
@@ -0,0 +1,22 @@
+package Project.Attendance.Backend.Controller;
+
+import Project.Attendance.Backend.Model.Student;
+import Project.Attendance.Backend.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping("/class/{classId}")
+    public List<Student> getStudentsByClass(@PathVariable Long classId){
+        return studentRepository.findByClassEntityId(classId);
+    }
+}
