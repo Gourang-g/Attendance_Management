a3ae215cc20ca7049776646f41f14b242f742785
Gourang-g <gouranggangrade382004@gmail.com>
2026-05-28 15:10:32 +0530

    updated files

---
+++ src/main/java/Project/Attendance/Backend/DTO/AdminRegisterDTO.java
@@ -0,0 +1,39 @@
+package Project.Attendance.Backend.DTO;
+
+public class AdminRegisterDTO {
+    private String username;
+    private String password;
+    private String name;
+
+    public AdminRegisterDTO() {}
+
+    public AdminRegisterDTO(String username, String password, String name) {
+        this.username = username;
+        this.password = password;
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
