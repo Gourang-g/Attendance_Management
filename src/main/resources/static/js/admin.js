30d6cbc95ed9d12ded134ce35811c0e4a1b8b5d9
Gourang-g <gouranggangrade382004@gmail.com>
2026-06-19 15:27:51 +0530

    updated files

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://onrender.com";
 if(localStorage.getItem("role") !== "ADMIN"){
     window.location.href = "index.html";
 }
--- src/main/resources/static/js/student.js
+++ src/main/resources/static/js/student.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://onrender.com";
 if(localStorage.getItem("role") !== "STUDENT"){
     window.location.href = "index.html";
 }
